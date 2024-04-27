3f08500f37541a45e3359f0966bb718bf410934c
ZhangJun <sn2015.zhang@gmail.com>
2024-04-28 04:50:29 +0800

    增加AI识别待办功能

--- pages/todoList/todoList.js
+++ pages/todoList/todoList.js
@@ -6,6 +6,56 @@ Page({
    * 页面的初始数据
    */
   data: {
+    animEa:'input',
+    animEb:'input',
+    animEc:'show',
+    showTodoSheet: false,
+    generateStatus: 'null',
+    hfsheetContent: "",
+    hfHeight:240,
+    generatedTodos:  [
+      {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }, {
+        "checked": true,
+        "name": "测试功能",
+        "value": 0
+      }
+    ],
     list: [{
         "text": "待办",
         "pagePath": "pages/todoList/todoList",
@@ -26,22 +76,30 @@ Page({
       }
     ],
     buttons: [{
-      text: '取1消'
-    }, {
-      text: '确1认'
-    }],
+        type: 'default',
+        className: '',
+        text: '智能识别',
+        value: 0
+      },
+      {
+        type: 'primary',
+        className: '',
+        text: '添加',
+        value: 1
+      }
+    ],
     todos: [{
       id: 1,
       content: '123',
-      done:false
+      done: false
     }, {
       id: 2,
       content: '1222223',
-      done:false
+      done: false
     }, {
       id: 3,
       content: '1嗯哼噶覆盖阿萨德挖了讨论收到货了说的话水电费23',
-      done:false
+      done: false
     }, ],
     stats: [{
       animB: '',
@@ -58,7 +116,12 @@ Page({
       animC: '',
       animD: '',
       selected: ''
-    }],
+    }]
+  },
+  bindinput: function (e) {
+    this.setData({
+      hfsheetContent: e.detail.value
+    })
   },
   saveTodo() {
     util.saveTodos(this.data.todos)
@@ -70,6 +133,11 @@ Page({
   },
   restart() {
     wx.exitMiniProgram()
+  },
+  aitest() {
+    this.setData({
+      showTodoSheet: true
+    })
   },
   handleTodoClick(target) {
     this.startClickAnimB(target);
@@ -92,9 +160,9 @@ Page({
         wx.showModal({
           title: '这条已经做完了嘛？',
           content: that.data.todos[target.mark.idx].content,
-          confirmText:'是的！',
-          cancelText:'还没有',
-          success (res) {
+          confirmText: '是的！',
+          cancelText: '还没有',
+          success(res) {
             if (res.confirm) {
               that.setData({
                 ['todos[' + target.mark.idx + '].done']: true
@@ -107,9 +175,9 @@ Page({
         wx.showModal({
           title: '修改待办',
           content: that.data.todos[target.mark.idx].content,
-          editable:true,
-          placeholderText:that.data.todos[target.mark.idx].content,
-          success (res) {
+          editable: true,
+          placeholderText: that.data.todos[target.mark.idx].content,
+          success(res) {
             if (res.confirm && res.content !== undefined && res.content.trim() !== "") {
               that.setData({
                 ['todos[' + target.mark.idx + '].content']: res.content
@@ -122,7 +190,7 @@ Page({
         wx.showModal({
           title: '要删除这条待办吗？',
           content: that.data.todos[target.mark.idx].content,
-          success (res) {
+          success(res) {
             if (res.confirm) {
               var todos = that.data.todos;
               todos.splice(target.mark.idx, 1)
@@ -136,6 +204,91 @@ Page({
       default:
         break;
     }
+  },
+  test(){
+    this.hfbtntap({detail:{index:1}})
+  },
+  requestTodoGenerate(prompt) {
+    var that = this
+    wx.request({
+      url: 'https://little.bushtit.cn/parse',
+      data: {
+        'msg': encodeURIComponent(this.data.hfsheetContent)
+      },
+      success(res) {
+        if (res.statusCode == 200) {
+          if (res.data && Array.isArray(res.data.todo) && res.data.todo.length != 0 && res.data.todo[0] != 'error') {
+            var gene = res.data.todo.map(function (item, index) {
+              return {
+                name: item,
+                value: index,
+                checked: true
+              }
+            })
+            that.setData({
+              generatedTodos: gene,
+              generateStatus: 'success'
+            })
+            return;
+          }
+          that.setData({
+            generateStatus: 'failed'
+          })
+        }
+      },
+      fail(res) {
+        that.setData({
+          generateStatus: 'failed'
+        })
+      }
+    })
+  },
+  hfbtntap(e) {
+    if (e.detail.index == 0) {
+      this.setData({generateStatus:'pending'})
+      this.requestTodoGenerate(this.data.hfsheetContent)
+    } else {
+      if(this.data.animEa==='input'){
+        this.setData({animEb:'',animEc:''})
+        setTimeout(() => {
+          this.setData({
+            animEa:'loading'
+          })
+        }, 200);setTimeout(() => {
+          this.setData({
+            animEb:'loading'
+          })
+        }, 220);
+      }else if(this.data.animEa==='loading'){
+        this.setData({animEb:'',animEc:'show'})
+        setTimeout(() => {
+          this.setData({
+            animEa:'result'
+          })
+        }, 200);setTimeout(() => {
+          this.setData({
+            animEb:'result'
+          })
+        }, 220);
+      }else{
+        this.setData({animEb:'',animEc:'show'})
+        setTimeout(() => {
+          this.setData({
+            animEa:'input'
+          })
+        }, 200);setTimeout(() => {
+          this.setData({
+            animEb:'input'
+          })
+        }, 220);
+      }
+      //this.doAddTodo(222, this.data.hfsheetContent)
+    }
+  },
+  hfclose() {
+    this.setData({
+      showTodoSheet: false
+    })
   },
   startClickAnimA() {
     this.setData({
@@ -194,7 +347,7 @@ Page({
     todos.push({
       id: id,
       content: content,
-      done:false
+      done: false
     })
     stats.push({
       animB: '',
@@ -226,7 +379,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-this.loadTodo()
+    this.loadTodo()
   },
 
   /**
@@ -277,31 +430,31 @@ this.loadTodo()
   onShareAppMessage() {
 
   },
-  onPageScroll(obj){
-    if(obj.scrollTop===0){
-      onTop=true;
+  onPageScroll(obj) {
+    if (obj.scrollTop === 0) {
+      onTop = true;
       wx.setNavigationBarColor({
         backgroundColor: '#fbf8ff',
         frontColor: '#000000',
-        fail(res){
+        fail(res) {
           console.log(res)
         },
-        animation:{
-          duration:300,
-          timingFunc:'easeOut'
+        animation: {
+          duration: 300,
+          timingFunc: 'easeOut'
         }
       })
-    }else if(onTop){
-      onTop=false;
+    } else if (onTop) {
+      onTop = false;
       wx.setNavigationBarColor({
         backgroundColor: '#F3EDF7',
         frontColor: '#000000',
-        fail(res){
+        fail(res) {
           console.log(res)
         },
-        animation:{
-          duration:300,
-          timingFunc:'easeOut'
+        animation: {
+          duration: 300,
+          timingFunc: 'easeOut'
         }
       })
     }
